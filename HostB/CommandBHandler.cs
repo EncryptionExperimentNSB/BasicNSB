a5ad1ba9bac1aa14445bea3a01860b85c2637351
JessWhite <jessica.white@oakbrookfinance.com>
2016-04-28 14:34:46 +0100

    inital commit

---
+++ HostB/CommandBHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HostA;
+using NServiceBus;
+using log4net;
+
+namespace HostB
+{
+    public class CommandBHandler : IHandleMessages<CommandB>
+    {
+        private readonly ILog logger = LogManager.GetLogger(typeof(CommandBHandler));
+        public void Handle(CommandB message)
+        {
+            logger.Info("Id: " + message.Id.ToString("D") + "DoB: " + message.DoB.ToString("yyyy-MM-dd HH:mm:ss") + "Name: " + message.Name);
+        }
+    }
+}
