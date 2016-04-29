7910f55d017ba4fed862d7f88b5ffba44485b0c0
JessWhite <jessica.white@oakbrookfinance.com>
2016-04-29 12:12:42 +0100

    Add Commandb

---
+++ HostA/CommandB.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HostA
+{
+   public class CommandB
+   {
+        public Guid Id { get; set; }
+        public DateTime DoB { get; set; }
+        public string Name { get; set; }
+   }
+}
